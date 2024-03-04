e66261029b6af3bfe4657bee893485e18941872b
Fernando Oliveira <fernandoaju78@gmail.com>
2024-03-03 21:55:30 -0300

    partition: Exclude ksu modules file from userdata backup
    Change-Id: I4b42f52ed9ef00e0c9c39b1de80bf47a2ade65cd
    Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>

--- partition.cpp
+++ partition.cpp
@@ -1258,6 +1258,7 @@ void TWPartition::Setup_Data_Media() {
 		backup_exclusions.add_absolute_dir("/data/cache");
         backup_exclusions.add_absolute_dir("/data/misc/apexdata/com.android.art"); // exclude this dir to prevent "error 255" on AOSP Android 12
 		backup_exclusions.add_absolute_dir("/data/extm"); //exclude this dir to prevent "error 255" on MIUI
+		backup_exclusions.add_absolute_dir("/data/adb/ksu/modules.img"); //After ksu 0.8.x the modules.img file became 1tb, which is inhibiting the execution of backups
 		wipe_exclusions.add_absolute_dir(Mount_Point + "/misc/vold"); // adopted storage keys
 		ExcludeAll(Mount_Point + "/system/storage.xml");
 
