ccf08002ae4183a9150e59c0db80e15c921d4494
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-06-10 16:53:59 +0800

    Use /persist/TWRP/.twrp_settings to save TWRP configuration

--- variables.h
+++ variables.h
@@ -20,7 +20,7 @@
 #define TW_MAIN_VERSION_STR       "3.7.1_12"
 #define TW_VERSION_STR TW_MAIN_VERSION_STR TW_DEVICE_VERSION
 #define TW_PERSIST_DIR              "/persist/TWRP"
-#define TW_SETTINGS_FILE            ".twrps_sk"
+#define TW_SETTINGS_FILE            ".twrp_settings"
 #define TW_RECOVERY_NAME            "TWRP"
 #define TW_DEFAULT_RECOVERY_FOLDER  "/" TW_RECOVERY_NAME
 #define TW_STORAGE_PATH             "/data/recovery/"
